489766958c069c149f1bbb8f403ffffe419c83f8
Fede <80333258+federicoprzybylski@users.noreply.github.com>
2022-12-01 18:35:27 -0300

    Delete ultrasonic.h

--- ultrasonic.h
+++
@@ -1,6 +0,0 @@
-#ifndef ultrasonic_h
-    #define ultrasonic_h
-    void setupUltrasonicPins(int trigPin, int echoPin);
-    int getCm(int trigPin, int echoPin);
-    int getInch(int trigPin, int echoPin);
-#endif
